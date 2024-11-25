d18acc39d41c63b2981bd3b337f3ec9a7d0ca851
jeremybepitech <jeremy.bisson@epitech.eu>
2024-11-25 16:22:29 +0100

    fix: conan

--- conanfile.py
+++
@@ -1,9 +0,0 @@
-from conan import ConanFile
-
-class MyConanFile(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    requires = [
-        "sfml/2.6.0",
-        "boost/1.83.0"
-    ]
-    generators = "CMakeDeps", "CMakeToolchain"
