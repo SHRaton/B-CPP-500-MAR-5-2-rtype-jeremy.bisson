12e82f182e0a17b41b88db1e135d4f4f8af149b1
MeddiGU <meddi.gueran@epitech.eu>
2025-01-15 12:07:20 +0100

    server: -Fix boss position (moonwalk)

--- src/lua/boss_ai.lua
+++ src/lua/boss_ai.lua
@@ -1,13 +1,24 @@
 function boss_ai(entity_id, position_x, position_y)
+    -- Définir les limites de la zone de mouvement
+    local min_x = 1200  -- Limite gauche
+    local max_x = 1700 -- Limite droite
     local speed = -5
-    if position_x < 1300 then
+
+    -- Si le boss est dans la zone acceptable, on le stoppe
+    if position_x <= max_x and position_x >= min_x then
         speed = 0
+    -- Si le boss est trop à gauche, on le fait revenir vers la droite
+    elseif position_x < min_x then
+        speed = 5
     end
+
+    -- Gestion du mouvement vertical
+    local vertical_speed = 0
     if position_y > 900 then
-        update_velocity(entity_id, speed, -5) -- Déplace en haut
+        vertical_speed = -5  -- Déplace en haut
     elseif position_y < 200 then
-        update_velocity(entity_id, speed, 5) -- Déplace en bas
-    else
-        update_velocity(entity_id, speed, 0)
+        vertical_speed = 5   -- Déplace en bas
     end
+
+    update_velocity(entity_id, speed, vertical_speed)
 end
